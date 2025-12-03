093675d9fb26a65de1ec35ceda1d8ea1753d6d39
Riyaz Ahmed <meesamriyaz@gmail.com>
2025-12-03 23:01:26 +0530

    added options for preflight support

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   // Localhost (frontend dev)
   'http://localhost:5173',
   'https://localhost:5173',
+  'http://10.0.2.2:5173',
+  'https://10.0.2.2:5173',
   'http://localhost:5174',
   'https://localhost:5174',
   'http://localhost:5175',
