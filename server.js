9f2697a556578b4bed3e350aaafc2482911d9de1
Riyaz Ahmed <meesamriyaz@gmail.com>
2025-12-05 07:31:16 +0530

    added trust proxy

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ import corsOptions from "./config/corsOptions.js";
 dotenv.config({ path: path.resolve(__dirname, ".env.local") });
 
 const app = express();
-
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5000;
 const NODE_ENV = process.env.NODE_ENV || "development";
 
