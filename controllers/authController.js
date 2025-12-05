cc856da28ce8a4575ec8956646e270d6fead2514
Riyaz Ahmed <meesamriyaz@gmail.com>
2025-12-05 20:55:28 +0530

    removed isProd

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,10 +67,9 @@ const setCookie = (res, accessToken, refreshToken) => {
     maxAge: 30 * 60 * 1000, // 30 minutes
   });
   res.cookie("refresh_token", refreshToken, {
-    httpOnly: isProd,
+    httpOnly: true,
     secure:  isProd,
     sameSite: "None",
-    path: "/api/auth/refresh-token",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 };
